efcff2f783f31aa92507b6bac78d137b091947bd
JarousNemec <jarous.nemec@gmail.com>
2023-03-22 21:21:06 +0100

    Initial commit

---
+++ LZW/LZWProcessor.cs
@@ -0,0 +1,57 @@
+﻿namespace LZW;
+
+public class LzwProcessor
+{
+    private Dictionary<int, string> _dictionary = new ();
+    private Dictionary<string, int> _reverseDictionary = new ();
+
+    public LzwProcessor()
+    {
+        for (int i = 'a'; i < 'z'; i++)
+        {
+            AddToDic(((char)i).ToString());
+        }
+        AddToDic("ab");
+        AddToDic("ba");
+    }
+
+    private void AddToDic(string value)
+    {
+        _dictionary.Add(_dictionary.Count, value);
+        _reverseDictionary.Add(value, _reverseDictionary.Count);
+    }
+    
+    public string Encode(string input)
+    {
+        string output = string.Empty;
+        
+        for (int i = 0; i < input.Length; i++)
+        {
+            char af = input[i];
+            // if ()
+            // {
+            //     
+            // }
+            
+        }
+        return output;
+    }
+
+    private string GetBiggestKnownString(string input, int i)
+    {
+        string output = string.Empty;
+        do
+        {
+            output += input[i];
+            i++;
+        } while (_dictionary.ContainsValue(output) && i < input.Length);
+        return output;
+    }
+
+    public string Decode(string input)
+    {
+        string output = string.Empty;
+
+        return output;
+    }
+}
---
+++ LZW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Channels;
+using LZW;
+
+Console.WriteLine("Hello, World!");
+var toEncode = "abababababababababababab".ToLower();
+var lzwProcessor = new LzwProcessor();
+var encoded = lzwProcessor.Encode(toEncode);
+var decoded = lzwProcessor.Decode(encoded);
+
+Console.WriteLine(toEncode);
+Console.WriteLine(encoded);
+Console.WriteLine(decoded);
