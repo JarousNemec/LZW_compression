fd4f9dc8751e3d5f8db71643bc1899e8e1641e74
JarousNemec <jarous.nemec@gmail.com>
2023-03-23 17:21:10 +0100

    done

--- LZW/LZWProcessor.cs
+++ LZW/LZWProcessor.cs
@@ -1,56 +1,115 @@
-﻿namespace LZW;
+﻿using System.Xml;
+
+namespace LZW;
 
 public class LzwProcessor
 {
-    private Dictionary<int, string> _dictionary = new ();
-    private Dictionary<string, int> _reverseDictionary = new ();
+    private Dictionary<int, string> _dictionary = new();
+    private Dictionary<string, int> _reverseDictionary = new();
 
-    public LzwProcessor()
+    private void InitializeDictionary()
     {
-        for (int i = 'a'; i < 'z'; i++)
+        _dictionary.Clear();
+        _reverseDictionary.Clear();
+        for (int i = 'a'; i <= 'z'; i++)
         {
             AddToDic(((char)i).ToString());
         }
-        AddToDic("ab");
-        AddToDic("ba");
     }
 
     private void AddToDic(string value)
     {
+        if (_reverseDictionary.ContainsKey(value)) return;
         _dictionary.Add(_dictionary.Count, value);
         _reverseDictionary.Add(value, _reverseDictionary.Count);
     }
-    
+
     public string Encode(string input)
     {
+        InitializeDictionary();
         string output = string.Empty;
-        
+
         for (int i = 0; i < input.Length; i++)
         {
-            char af = input[i];
-            // if ()
-            // {
-            //     
-            // }
-            
+            if (output != string.Empty)
+            {
+                output += ";";
+            }
+
+            var known = GetBiggestKnownString(input, i, input.Length, out int nextIndex);
+            i = nextIndex;
+            output += _reverseDictionary[known];
         }
+
         return output;
     }
 
-    private string GetBiggestKnownString(string input, int i)
+
+    private string GetBiggestKnownString(string input, int i, int maxIndex, out int nextIndex)
     {
-        string output = string.Empty;
-        do
+        string temp = String.Empty;
+        string output = input[i].ToString();
+        if (i == maxIndex - 1)
         {
-            output += input[i];
-            i++;
-        } while (_dictionary.ContainsValue(output) && i < input.Length);
-        return output;
+            nextIndex = i;
+            return output;
+        }
+
+        temp = input[i + 1].ToString();
+
+        while (true)
+        {
+            if (_dictionary.ContainsValue(output + temp))
+            {
+                output += temp;
+                i++;
+                if (i == maxIndex)
+                {
+                    nextIndex = i;
+                    return output;
+                }
+
+                temp = input[i].ToString();
+            }
+            else
+            {
+                AddToDic(output + temp);
+                nextIndex = i;
+                return output;
+            }
+        }
     }
 
     public string Decode(string input)
     {
-        string output = string.Empty;
+        InitializeDictionary();
+        var output = string.Empty;
+        var actual = string.Empty;
+        var last = string.Empty;
+        var keys = input.Split(';');
+        foreach (var t in keys)
+        {
+            checkAgain:
+            actual = t;
+            if (_dictionary.ContainsKey(Convert.ToInt32(actual)))
+            {
+                output += _dictionary[Convert.ToInt32(actual)];
+                if (last != string.Empty)
+                {
+                    AddToDic(last + _dictionary[Convert.ToInt32(actual)][0]);
+                }
+            }
+            else
+            {
+                if (last != string.Empty)
+                {
+                    AddToDic(last + last[0]);
+                    goto checkAgain;
+                }
+            }
+
+            last = _dictionary[Convert.ToInt32(actual)];
+        }
 
         return output;
     }
--- LZW/Program.cs
+++ LZW/Program.cs
@@ -2,10 +2,20 @@
 using LZW;
 
 Console.WriteLine("Hello, World!");
-var toEncode = "abababababababababababab".ToLower();
+var toEncode = "abababab".ToLower();
 var lzwProcessor = new LzwProcessor();
 var encoded = lzwProcessor.Encode(toEncode);
 var decoded = lzwProcessor.Decode(encoded);
+
+Console.WriteLine(toEncode);
+Console.WriteLine(encoded);
+Console.WriteLine(decoded);
+
+Console.WriteLine("---------------------------------------");
+
+toEncode = "abcdabcdabcdabcdabcdabcdahojahojahojahoj".ToLower();
+encoded = lzwProcessor.Encode(toEncode);
+decoded = lzwProcessor.Decode(encoded);
 
 Console.WriteLine(toEncode);
 Console.WriteLine(encoded);
